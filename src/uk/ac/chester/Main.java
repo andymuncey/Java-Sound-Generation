c4a63cb4a4fde5a0886a39c23f9457c7e68e8b77
Andrew <a.muncey@chester.ac.uk>
2019-05-28 16:23:13 +0100

    Initial commit

---
+++ src/uk/ac/chester/Main.java
@@ -0,0 +1,29 @@
+package uk.ac.chester;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+
+        playScale(new Octave().minor());
+    }
+
+
+    static void playScale(double[] scaleNotes) throws Exception{
+
+        ToneGenerator generator = new ToneGenerator();
+
+        for (double freq : scaleNotes){
+            generator.play(freq);
+            Thread.sleep(250);
+        }
+
+        for (int i = scaleNotes.length-1; i >=0 ; i--) {
+            generator.play(scaleNotes[i]);
+            Thread.sleep(250);
+        }
+        generator.stop();
+    }
+
+
+}
---
+++ src/uk/ac/chester/ToneGenerator.java
@@ -0,0 +1,58 @@
+package uk.ac.chester;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.SourceDataLine;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ToneGenerator {
+
+    private void setFrequency(double frequency){
+        frequencyAsLong = (Double.doubleToLongBits(frequency));
+    }
+
+    private long frequencyAsLong;
+
+    private boolean running = false;
+
+    void stop() {
+        running = false;
+    }
+
+    void play(double newFrequency) {
+        setFrequency(newFrequency);
+
+        if (!running) {
+            running = true;
+
+            Thread thread = new Thread(() -> {
+                float sampleRate = 44100;
+
+                byte[] buf = new byte[1];
+                AudioFormat audioFormat = new AudioFormat(sampleRate, 8, 1, true, false);
+
+                try {
+                    SourceDataLine sdl = AudioSystem.getSourceDataLine(audioFormat);
+                    sdl.open();
+                    sdl.start();
+
+                    int volume = 127; //cant exceed 127 or will overflow the byte once multiplied by the magnitude of the wave
+
+                    for (int i = 0; running; i++) {
+                        double frequency = Double.longBitsToDouble(frequencyAsLong);
+                        double angle = i / (sampleRate / frequency) * 2.0 * Math.PI;
+                        buf[0] = (byte) (Math.sin(angle) * volume); //Math.sin() returns value between -1 and 1, may byte value is 127
+                        sdl.write(buf, 0, 1);
+                    }
+                    sdl.drain();
+                    sdl.stop();
+                } catch (Exception ignored) {
+                }
+            });
+
+            thread.start();
+        }
+    }
+
+}
