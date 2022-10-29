113b2ea31bdd1c602a74bf57e38931f5444a3ec3
Andrew <a.muncey@chester.ac.uk>
2022-10-29 17:25:12 +0100

    new tune

--- src/com/tinyappco/Main.java
+++ src/com/tinyappco/Main.java
@@ -3,6 +3,7 @@ package com.tinyappco;
 
 import com.tinyappco.synths.SineWave;
 import com.tinyappco.synths.Synthesizer;
+import com.tinyappco.temperaments.EqualTemperament;
 import com.tinyappco.temperaments.JustIntonation;
 
 public class Main {
@@ -10,15 +11,62 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Synthesizer synth = new SineWave();
-        FrequencyFinder temperament = new FrequencyFinder(new JustIntonation());
+        FrequencyFinder temperament = new FrequencyFinder(new EqualTemperament());
 
         SoundGenerator generator = new SoundGenerator(synth, temperament);
 
-        generator.play(Note.parse("C4"), 2000, Velocity.PP);
-        generator.play(Note.parse("G4"), 2000, Velocity.PP);
-        generator.play(Note.parse("C5"), 3000, Velocity.PP);
-        generator.play(Note.parse("E5"), 250, Velocity.FFF);
-        generator.play(Note.parse("Eb5"), 1750, Velocity.FFF);
+//        generator.play(Note.parse("C4"), 2000, Velocity.PP);
+//        generator.play(Note.parse("G4"), 2000, Velocity.PP);
+//        generator.play(Note.parse("C5"), 3000, Velocity.PP);
+//        generator.play(Note.parse("E5"), 250, Velocity.FFF);
+//        generator.play(Note.parse("Eb5"), 1750, Velocity.FFF);
+
+        generator.play(Note.parse("G#4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+        generator.play(Note.parse("G#4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+
+
+        generator.play(Note.parse("A4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+        generator.play(Note.parse("A4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+
+        generator.play(Note.parse("G#4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+        generator.play(Note.parse("G#4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+
+        generator.play(Note.parse("F#4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+        generator.play(Note.parse("F#4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+
+        generator.play(Note.parse("B4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+        generator.play(Note.parse("B4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+
+        generator.play(Note.parse("A4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+        generator.play(Note.parse("A4"), 250, Velocity.MF);
+        generator.play(Note.parse("C#4"), 250, Velocity.MF);
+
+        generator.play(Note.parse("G#4"), 250, Velocity.MF);
+        generator.play(Note.parse("D#4"), 250, Velocity.MF);
+        generator.play(Note.parse("G#4"), 250, Velocity.MF);
+        generator.play(Note.parse("D#4"), 250, Velocity.MF);
+
+        generator.play(Note.parse("G#4"), 125, Velocity.MF);
+        generator.play(Note.parse("A4"), 125, Velocity.MF);
+        generator.play(Note.parse("G#4"), 125, Velocity.MF);
+        generator.play(Note.parse("F#4"), 125, Velocity.MF);
+        generator.play(Note.parse("G#4"), 125, Velocity.MF);
+        generator.play(Note.parse("F#4"), 125, Velocity.MF);
+
+        generator.play(Note.parse("E4"), 125, Velocity.MF);
+        generator.play(Note.parse("D#4"), 125, Velocity.MF);
+        generator.play(Note.parse("C#4"), 500, Velocity.MF);
 
 //        File file = new File("src/ThoughtContagion.txt");
 //        FileInputStream fileInputStream = new FileInputStream(file);
